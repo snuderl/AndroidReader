96cfc189872ffd914a37f5887bde8ae37338f0de
Snuderl <Snuderl@gmail.com>
2012-06-08 14:02:41 +0200

    Web view

--- src/org/snuderl/DetailsActivity.java
+++ src/org/snuderl/DetailsActivity.java
@@ -3,10 +3,12 @@ package org.snuderl;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 public class DetailsActivity extends Activity{
-	TextView title, pubDate, Category, content;
+	TextView title, pubDate, Category;
+	WebView content;
 	NewsMessage news = null;
 
 	@Override
@@ -19,14 +21,19 @@ public class DetailsActivity extends Activity{
 		title = (TextView)findViewById(R.id.detailsTitle);
 		pubDate = (TextView)findViewById(R.id.detailsPubDate);
 		Category = (TextView)findViewById(R.id.detailsCategory);
-		content = (TextView)findViewById(R.id.detailsContent);
+		content = (WebView)findViewById(R.id.detailsContent);
 		
 		title.setText(news.Title);
 		pubDate.setText(news.Date);
 		
 		//Zamenjano. WTF
 		Category.setText(news.Category);
-		content.setText(news.Content);
+		String body =  "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"+
+			       "<html><head>"+
+			       "<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />"+
+			       "<head><body>"
+			       +news.Content+"</body></html>";
+		content.loadData(body, "text/html; charset=utf-8", "UTF-8");
 		
 		
 	}
